b843c37516c54f92086f6a0c11fe79bff655cdce
Mark Cacal <Kram@DESKTOP-PJQQAR8>
2018-04-26 20:12:07 +0800

    Connectivity Checker

--- Jarcet.Mobile/Jarcet.Mobile.Android/MainActivity.cs
+++ Jarcet.Mobile/Jarcet.Mobile.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Plugin.Connectivity;
+using Xamarin.Forms;
 
 namespace Jarcet.Mobile.Droid
 {
@@ -18,10 +21,22 @@ namespace Jarcet.Mobile.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-            
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
-         
+            var view = ((Activity)Forms.Context).FindViewById(Android.Resource.Id.Content);
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+
+                Snackbar.Make(view, "You dont have internet connection", 600).Show();
+            }
+
+            CrossConnectivity.Current.ConnectivityTypeChanged += async (s, e) =>
+            {
+                if (!e.IsConnected)
+                    Snackbar.Make(view, "You dont have internet connection", 600).Show();
+
+            };
         }
     }
 }
